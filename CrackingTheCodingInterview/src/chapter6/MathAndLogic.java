2024eead4921d0886af41d191e4ae10fdc4ac9ff
Ziwen Guan <ziweninus@gmail.com>
2016-06-16 10:00:47 -0400

    cracking the coding interview
    chapter 6 math and logic

---
+++ CrackingTheCodingInterview/src/chapter6/MathAndLogic.java
@@ -0,0 +1,10 @@
+package chapter6;
+
+public class MathAndLogic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ CrackingTheCodingInterview/src/chapter6/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Cracking the coding interview
+ * Chapter 6 Math and Logic Puzzles
+ * There are no coding questions from book
+ */
+/**
+ * @author zg55
+ *
+ */
+package chapter6;
